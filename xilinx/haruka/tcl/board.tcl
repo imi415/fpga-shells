390ffec5c5838da9d2dff36448e764b71605a0cb
imi415 <imi415.public@gmail.com>
2019-05-07 18:27:29 +0800

    Fixed part name.

--- xilinx/haruka/tcl/board.tcl
+++ xilinx/haruka/tcl/board.tcl
@@ -1,5 +1,5 @@
 # See LICENSE for license details.
 set name {haruka}
-set part_fpga {xc7a35tftg256c-1}
+set part_fpga {xc7a35tftg256-1}
 set part_board {imi.moe:haruka:part0:1.1}
 set bootrom_inst {rom}
